a146ad161e4b0e1033d030bff4feab979f230f68
Rick Mohamed <rickmohaa@gmail.com>
2023-06-22 15:08:32 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let surprise = "surprise";
+
+const writeCards=(names, surprise)=>{
+    let newArr=[];
+    for(let i=0; i<names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr
+}
+
+writeCards(names);
+
+
+
+const countDown=(names, surprise)=>{
+    for(let i=0; i<11; i++){
+        console.log(i);
+    }
+}
+
+countDown();
+
+
+
